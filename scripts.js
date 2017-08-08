f7826d36594017a40252f5f3dab646ed1bd45ed5
tommytnez <tommytnez@yahoo.com>
2017-08-07 17:56:09 -0700

    update efarmony

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
 //Use this file to implement Part One of your project
+'use strict';
 
+let animal = {};
 
+animal.username = 'kingKong';
+
+animal['tagline'] = 'i am king of kongs';
+
+var noises = [];
+animal.noises = noises;
+
+var count = 0;
+for(var key in animal){
+  count++;
+  if(key === 'username'){
+    console.log('hi my name is ' + animal[key]);
+  }
+  if(key === 'tagline'){
+    console.log('i like to say ' + animal[key]);
+  }
+}
+console.log(count);
